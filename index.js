48ea629c1e019e22cd95298360529f66e5ab9200
shannonzylstra <shannonzylstra@gmail.com>
2020-01-02 15:16:00 -0800

    Added more files.

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+let server = require('http').createServer(app);
+let io = require('socket.io').listen(server);
 const layout = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(layout);
+
+// SOCKET VARIABLES
+let connections = [];
+let players = [];
+// SOCKET STUFF
+io.sockets.on('connection', socket => {
+    connections.push(socket);
+    console.log(`Connections made: ${connections.length} sockets connected`);
+
+    socket.on('disconnect', data => {
+        players.splice(players.indexOf(socket.username), 1);
+        updateUsernames();
+        console.log(data);
+        connections.splice(connections.indexOf(socket), 1);
+        io.emit('disconnected', socket.username);
+        console.log(`Disconnected: ${connections.length} sockets connected.`);
+    });
+
+    const updateUsernames = () => {
+        
+    }
+});
 
 app.get('/', (req, res) => {
     res.render('index');
